c2eee946811e48cbfd3662c91b78a4aa51f57712
ccqw3ewk <lehaksay@gmail.com>
2025-12-04 00:54:03 +0300

    rework and add "Регистрация" text

--- sportguider/lib/presentation/pages/regPage/reg_page.dart
+++ sportguider/lib/presentation/pages/regPage/reg_page.dart
@@ -2,6 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:sportguider/presentation/pages/regPage/widgets/password_input_field.dart';
 import 'package:sportguider/presentation/pages/regPage/widgets/login_input_field.dart';
+import 'package:sportguider/presentation/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 @RoutePage()
 class RegPage extends StatefulWidget {
   const RegPage({super.key});
@@ -14,31 +16,55 @@ class _RegPageState extends State<RegPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: 60,
-          right: 60,
-          top: 325,
-        ),
-        child: Column(
-          children: [
-            //Виджет-логин
-            Container(
-              width: 320,
-              height: 35,
-              child: LoginInputField(),
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: 60,
+              right: 60,
+              top: 325,
             ),
-            
-            SizedBox(height: 100), //Отступ между виджетами
-            
-            // Виджет-пароль
-            Container(
-              width: 320,
-              height: 35,
-              child: PasswordInputField(),
+            child: Column(
+              children: [
+
+                SizedBox(height: 60),
+
+                //Виджет-логин
+                Container(
+                  width: 320,
+                  height: 35,
+                  child: LoginInputField(),
+                ),
+                
+                SizedBox(height: 100),
+                
+                //Виджет-пароль
+                Container(
+                  width: 320,
+                  height: 35,
+                  child: PasswordInputField(),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          
+          //Надпись "Регистрация"
+          Positioned(
+            top: 200,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                'Регистрация',
+                style: GoogleFonts.philosopher(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.activeColor,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
