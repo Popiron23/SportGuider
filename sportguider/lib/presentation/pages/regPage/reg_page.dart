dfda38aacfe9964a386210dc34555a20e58314c0
ccqw3ewk <lehaksay@gmail.com>
2025-12-03 23:28:56 +0300

    adding a login widget and adjusting both widgets on the screen

--- sportguider/lib/presentation/pages/regPage/reg_page.dart
+++ sportguider/lib/presentation/pages/regPage/reg_page.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:sportguider/presentation/pages/regPage/widgets/password_input_field.dart';
-
+import 'package:sportguider/presentation/pages/regPage/widgets/login_input_field.dart';
 @RoutePage()
 class RegPage extends StatefulWidget {
   const RegPage({super.key});
@@ -11,23 +11,35 @@ class RegPage extends StatefulWidget {
 }
 
 class _RegPageState extends State<RegPage> {
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Padding(
-      padding: EdgeInsets.only(
-       left:60,
-       right:60,
-       top:100
-      ),
-      child: Center( // Центрируем по горизонтали
-        child: Container(
-          width: 320,
-          height: 35,
-          child: PasswordInputField(),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 60,
+          right: 60,
+          top: 325,
+        ),
+        child: Column(
+          children: [
+            //Виджет-логин
+            Container(
+              width: 320,
+              height: 35,
+              child: LoginInputField(),
+            ),
+            
+            SizedBox(height: 100), //Отступ между виджетами
+            
+            // Виджет-пароль
+            Container(
+              width: 320,
+              height: 35,
+              child: PasswordInputField(),
+            ),
+          ],
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
