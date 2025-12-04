b86484b16ebe18153847814f776bbc762475f528
Popiron23 <galushko.ant@yandex.ru>
2025-12-04 21:13:23 +0300

    add nav map auth

--- sportguider/lib/routes/router.dart
+++ sportguider/lib/routes/router.dart
@@ -6,13 +6,20 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
     AutoRoute(
-      path: '/home',
-      page: HomeRoute.page,
-      initial: true,
+      path: '/',
+      page: RootRoute.page,
       children: [
-        AutoRoute(path: 'map', page: MapRoute.page),
-        AutoRoute(path: 'reg', page: CoachRoute.page),
-        AutoRoute(path: 'friend', page: FriendRoute.page),
+        AutoRoute(
+          path: 'home',
+          page: HomeRoute.page,
+          initial: true,
+          children: [
+            AutoRoute(path: 'map', page: MapRoute.page),
+            AutoRoute(path: 'reg', page: CoachRoute.page),
+            AutoRoute(path: 'friend', page: FriendRoute.page),
+          ],
+        ),
+        AutoRoute(path: 'auth', page: AuthRoute.page),
       ],
     ),
   ];
--- sportguider/lib/routes/router.gr.dart
+++ sportguider/lib/routes/router.gr.dart
@@ -9,56 +9,111 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:collection/collection.dart' as _i8;
-import 'package:flutter/material.dart' as _i6;
-import 'package:sportguider/domain/entities/location_entity.dart' as _i7;
-import 'package:sportguider/presentation/pages/friendPage/friend_page.dart'
-    as _i1;
-import 'package:sportguider/presentation/pages/mapPage/map_page.dart' as _i3;
+import 'package:auto_route/auto_route.dart' as _i10;
+import 'package:collection/collection.dart' as _i13;
+import 'package:flutter/material.dart' as _i11;
+import 'package:sportguider/domain/entities/location_entity.dart' as _i12;
+import 'package:sportguider/presentation/pages/authPage/auth_page.dart' as _i1;
 import 'package:sportguider/presentation/pages/coachPage/coach_page.dart'
+    as _i2;
+import 'package:sportguider/presentation/pages/coachProfilePage/coachProfile_page.dart'
+    as _i3;
+import 'package:sportguider/presentation/pages/friendPage/friend_page.dart'
     as _i4;
-import 'package:sportguider/presentation/widgets/home_page.dart' as _i2;
+import 'package:sportguider/presentation/pages/mapPage/map_page.dart' as _i6;
+import 'package:sportguider/presentation/pages/regPage/reg_page.dart' as _i7;
+import 'package:sportguider/presentation/pages/root_page.dart' as _i8;
+import 'package:sportguider/presentation/pages/userProfilePage/userProfile_page.dart'
+    as _i9;
+import 'package:sportguider/presentation/widgets/home_page.dart' as _i5;
 
 /// generated route for
-/// [_i1.FriendPage]
-class FriendRoute extends _i5.PageRouteInfo<void> {
-  const FriendRoute({List<_i5.PageRouteInfo>? children})
+/// [_i1.AuthPage]
+class AuthRoute extends _i10.PageRouteInfo<void> {
+  const AuthRoute({List<_i10.PageRouteInfo>? children})
+    : super(AuthRoute.name, initialChildren: children);
+
+  static const String name = 'AuthRoute';
+
+  static _i10.PageInfo page = _i10.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.AuthPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i2.CoachPage]
+class CoachRoute extends _i10.PageRouteInfo<void> {
+  const CoachRoute({List<_i10.PageRouteInfo>? children})
+    : super(CoachRoute.name, initialChildren: children);
+
+  static const String name = 'CoachRoute';
+
+  static _i10.PageInfo page = _i10.PageInfo(
+    name,
+    builder: (data) {
+      return const _i2.CoachPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i3.CoachProfilePage]
+class CoachProfileRoute extends _i10.PageRouteInfo<void> {
+  const CoachProfileRoute({List<_i10.PageRouteInfo>? children})
+    : super(CoachProfileRoute.name, initialChildren: children);
+
+  static const String name = 'CoachProfileRoute';
+
+  static _i10.PageInfo page = _i10.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.CoachProfilePage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i4.FriendPage]
+class FriendRoute extends _i10.PageRouteInfo<void> {
+  const FriendRoute({List<_i10.PageRouteInfo>? children})
     : super(FriendRoute.name, initialChildren: children);
 
   static const String name = 'FriendRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i10.PageInfo page = _i10.PageInfo(
     name,
     builder: (data) {
-      return const _i1.FriendPage();
+      return const _i4.FriendPage();
     },
   );
 }
 
 /// generated route for
-/// [_i2.HomePage]
-class HomeRoute extends _i5.PageRouteInfo<void> {
-  const HomeRoute({List<_i5.PageRouteInfo>? children})
+/// [_i5.HomePage]
+class HomeRoute extends _i10.PageRouteInfo<void> {
+  const HomeRoute({List<_i10.PageRouteInfo>? children})
     : super(HomeRoute.name, initialChildren: children);
 
   static const String name = 'HomeRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i10.PageInfo page = _i10.PageInfo(
     name,
     builder: (data) {
-      return const _i2.HomePage();
+      return const _i5.HomePage();
     },
   );
 }
 
 /// generated route for
-/// [_i3.MapPage]
-class MapRoute extends _i5.PageRouteInfo<MapRouteArgs> {
+/// [_i6.MapPage]
+class MapRoute extends _i10.PageRouteInfo<MapRouteArgs> {
   MapRoute({
-    _i6.Key? key,
-    required List<_i7.LocationEntity> locations,
-    List<_i5.PageRouteInfo>? children,
+    _i11.Key? key,
+    required List<_i12.LocationEntity> locations,
+    List<_i10.PageRouteInfo>? children,
   }) : super(
          MapRoute.name,
          args: MapRouteArgs(key: key, locations: locations),
@@ -67,11 +122,11 @@ class MapRoute extends _i5.PageRouteInfo<MapRouteArgs> {
 
   static const String name = 'MapRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i10.PageInfo page = _i10.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<MapRouteArgs>();
-      return _i3.MapPage(key: args.key, locations: args.locations);
+      return _i6.MapPage(key: args.key, locations: args.locations);
     },
   );
 }
@@ -79,9 +134,9 @@ class MapRoute extends _i5.PageRouteInfo<MapRouteArgs> {
 class MapRouteArgs {
   const MapRouteArgs({this.key, required this.locations});
 
-  final _i6.Key? key;
+  final _i11.Key? key;
 
-  final List<_i7.LocationEntity> locations;
+  final List<_i12.LocationEntity> locations;
 
   @override
   String toString() {
@@ -93,7 +148,7 @@ class MapRouteArgs {
     if (identical(this, other)) return true;
     if (other is! MapRouteArgs) return false;
     return key == other.key &&
-        const _i8.ListEquality<_i7.LocationEntity>().equals(
+        const _i13.ListEquality<_i12.LocationEntity>().equals(
           locations,
           other.locations,
         );
@@ -102,21 +157,53 @@ class MapRouteArgs {
   @override
   int get hashCode =>
       key.hashCode ^
-      const _i8.ListEquality<_i7.LocationEntity>().hash(locations);
+      const _i13.ListEquality<_i12.LocationEntity>().hash(locations);
 }
 
 /// generated route for
-/// [_i4.CoachPage]
-class CoachRoute extends _i5.PageRouteInfo<void> {
-  const CoachRoute({List<_i5.PageRouteInfo>? children})
-    : super(CoachRoute.name, initialChildren: children);
+/// [_i7.RegPage]
+class RegRoute extends _i10.PageRouteInfo<void> {
+  const RegRoute({List<_i10.PageRouteInfo>? children})
+    : super(RegRoute.name, initialChildren: children);
 
-  static const String name = 'CoachRoute';
+  static const String name = 'RegRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i10.PageInfo page = _i10.PageInfo(
     name,
     builder: (data) {
-      return const _i4.CoachPage();
+      return const _i7.RegPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i8.RootPage]
+class RootRoute extends _i10.PageRouteInfo<void> {
+  const RootRoute({List<_i10.PageRouteInfo>? children})
+    : super(RootRoute.name, initialChildren: children);
+
+  static const String name = 'RootRoute';
+
+  static _i10.PageInfo page = _i10.PageInfo(
+    name,
+    builder: (data) {
+      return const _i8.RootPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i9.UserProfilePage]
+class UserProfileRoute extends _i10.PageRouteInfo<void> {
+  const UserProfileRoute({List<_i10.PageRouteInfo>? children})
+    : super(UserProfileRoute.name, initialChildren: children);
+
+  static const String name = 'UserProfileRoute';
+
+  static _i10.PageInfo page = _i10.PageInfo(
+    name,
+    builder: (data) {
+      return const _i9.UserProfilePage();
     },
   );
 }
