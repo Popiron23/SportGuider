5e02e65a8470eba40955bc38c92a39f2e3207ea0
Franchesko de Parmesano <84846891+ponchikSmyasom@users.noreply.github.com>
2025-12-05 19:52:27 +0300

    добавлена страница загрузки

---
+++ sportguider/lib/presentation/pages/splashScreen/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sportguider/presentation/colors.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            'sportguider/assets/images/icon.svg',
+            colorFilter: ColorFilter.mode(
+              AppColors.activeColor,
+              BlendMode.srcIn,
+            ),
+          ),
+          SpinKitCircle(color: Colors.blue, size: 50.0),
+        ],
+      ),
+    );
+  }
+}
