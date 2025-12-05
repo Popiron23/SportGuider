dba6f88343b7105e8c023aa3ab95d9130f226ff5
Luvaatym <telyatnikmaria08@icloud.com>
2025-12-05 19:14:54 +0300

    changing the color of the registration page

--- sportguider/lib/presentation/pages/regPage/reg_page.dart
+++ sportguider/lib/presentation/pages/regPage/reg_page.dart
@@ -19,6 +19,7 @@ class _RegPageState extends State<RegPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:Colors.white,
       body: Stack(
         children: [
           Padding(
