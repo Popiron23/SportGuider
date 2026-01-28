732f86ef8bae606c35687f0f50a16423e8173381
Franchesko de Parmesano <84846891+ponchikSmyasom@users.noreply.github.com>
2026-01-28 22:19:28 +0300

    подключен firebase

--- sportguider/lib/main.dart
+++ sportguider/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sportguider/presentation/app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
