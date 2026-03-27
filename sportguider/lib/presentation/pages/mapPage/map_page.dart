534496868bc4461cf94b544313819b0e38504c81
maria <telyatnikmaria@yandex.ru>
2026-03-27 18:20:09 +0300

    add camera position

--- sportguider/lib/presentation/pages/mapPage/map_page.dart
+++ sportguider/lib/presentation/pages/mapPage/map_page.dart
@@ -120,7 +120,7 @@ class _MapPageState extends State<MapPage> {
     List<MapObject> mapObjects = [];
     final largeMapObject = ClusterizedPlacemarkCollection(
       mapId: largeMapObjectId,
-      radius: 110,
+      radius: 60,
       minZoom: 15,
       onClusterAdded:
           (ClusterizedPlacemarkCollection self, Cluster cluster) async {
@@ -230,8 +230,19 @@ class _MapPageState extends State<MapPage> {
                       return YandexMap(
                         key: mapKey,
                         mapObjects: _updateLocations(state.locations),
-                        onMapCreated: (controller) {
+                        onMapCreated: (controller) async {
                           mapController = controller;
+                          await controller.moveCamera(
+                            CameraUpdate.newCameraPosition(
+                              const CameraPosition(
+                                target: Point(
+                                  latitude: 47.2357,
+                                  longitude: 39.7015,
+                                ),
+                                zoom: 11,
+                              ),
+                            ),
+                          );
                         },
                         onUserLocationAdded: (UserLocationView view) async {
                           return view.copyWith(
