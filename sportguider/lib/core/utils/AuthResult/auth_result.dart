aacbf4dc4cfad6c2617fb13af06ed66a713c81a5
Franchesko de Parmesano <84846891+ponchikSmyasom@users.noreply.github.com>
2026-05-08 10:53:45 +0300

    Исправить regex валидации email

--- sportguider/lib/core/utils/AuthResult/auth_result.dart
+++ sportguider/lib/core/utils/AuthResult/auth_result.dart
@@ -49,7 +49,7 @@ class AuthResult {
 
     // Basic "good enough" email format check.
     final basicRegex = RegExp(
-      r'^[A-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[A-Z0-9-]+(?:\.[A-Z0-9-]+)*\.[A-Z]{2,}$',
+      r"^[A-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9-]+(?:\.[A-Z0-9-]+)*\.[A-Z]{2,}$",
       caseSensitive: false,
     );
 
